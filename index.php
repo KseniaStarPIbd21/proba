2dfb56d98d83f837935f1e5338eaa24469f8b953
KseniaStarPIbd21 <anorecsichka@yandex.ru>
2017-10-08 20:29:27 +0400

    design, empty page

---
+++ index.php
@@ -0,0 +1,48 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Фотостудия "бла-бла". Главная.</title>
+<link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+<!-- <h2>Меню:</h2>
+<a href="index.html">Всё о слонах.</a>
+<a href="elephant.html">Купить слона.</a>
+<a href="elephant1.html">Взять слона на прокат.</a>
+<div id="navigation">
+    <table id="menu">
+        <tr>
+            <td><a href="index.php"  title="">Главная</a></td>
+            <td>
+			<select>
+  <option>Пункт 1</option>
+  <option>Пункт 2</option>
+</select>
+			</td>
+            <td><a href="about.php"  title="">Обо мне</a></td>
+            <td><a href="book.php"  title="">Записаться</a></td>
+            <td><a href="contacts.php"  title="">Контакты</a></td>
+        </tr>
+    </table>
+</div>
+-->
+<header>
+  <a href="" class="logo">Фотостудия "бла-бла"</a>
+  <nav>
+      <ul class="topmenu">
+        <li><a href="index.php">Главная</a></li>
+        <li><a href="" class="submenu-link">О студии</a>
+          <ul class="submenu">
+            <li><a href="zagluschka.php">Оборудование</a></li>
+            <li><a href="">Правила студии</a></li>
+          </ul>
+        </li>
+		 <li><a href="zagluschka.php">Интерьеры и цены</a></li>
+        <li><a href="bron.php">Бронирование</a></li>
+        <li><a href="zagluschka.php">Контакты</a></li>
+      </ul>
+    </nav>
+</header>
+
+</body>
+</html>
---
+++ zagluschka.php
@@ -0,0 +1,14 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Фотостудия "бла-бла". </title>
+<link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+<header>
+ <?php include 'index.php' ?>
+ 
+</header>
+<p>Данная страница пока находится в разработке. </p>
+</body>
+</html>
